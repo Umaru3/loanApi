ae7107804fff0c25659639d699adbb126a4b3de9
VEV\justi <justincastro105@gmail.com>
2026-03-23 23:48:06 +0800

    userBranch-001: [FEATURE][ALL] - setup proj

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        console.log('Connecting to MongoDB...');
+        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        console.log('MongoDB connected:', conn.connection.host);
+        
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+}
